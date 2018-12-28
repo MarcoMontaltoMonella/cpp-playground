4857050d334d82916fc25daf51db3e14587901ba
Marco Montalto Monella <mmm1108@nyu.edu>
2018-12-28 12:50:48 +0100

    Add example of a class

---
+++ class-example.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+const int STACK_SIZE = 100;
+
+class stack {
+    private:
+        int count;
+        int data[STACK_SIZE];
+    
+    public:
+        void init(void);
+        void push(const int item);
+        int pop(void);
+
+};
+
+inline void stack::init(void)
+{
+    count = 0;
+}
+
+inline void stack::push(const int item)
+{
+    data[count] = item;
+    ++count;
+}
+
+inline int stack::pop(void)
+{
+    --count;
+    return (data[count]);
+}
+
+int main() 
+{
+    stack a_stack;
+    a_stack.init();
+
+    a_stack.push(10);
+    a_stack.push(45);
+    a_stack.push(23);
+
+    cout << a_stack.pop() << "\n";
+    cout << a_stack.pop() << "\n";
+    cout << a_stack.pop() << "\n";
+
+    return 0;
+}
